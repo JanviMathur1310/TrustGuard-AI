2811732429e612ccb16b05a4f7e4f673c0d5a49f
Janvi Mathur <janvimathur75@gmail.com>
2026-08-25 23:57:55 +0530

    Allow GitHub Pages frontend

--- backend/main.py
+++ backend/main.py
@@ -20,9 +20,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://janvimathur1310.github.io"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
